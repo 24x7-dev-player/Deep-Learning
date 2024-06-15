a1eb530f337ce2d64ab033e913b5d6de71b0a13c
🧐 ᴛᴜꜱʜᴀʀ-ᴘᴇʀꜱᴘᴇᴄᴛɪᴠᴇ <61559891+TUSHAR-PERSPECTIVE@users.noreply.github.com>
2024-06-16 03:38:28 +0530

    commit

---
+++ esrgan_(enhanced_srgan).py
@@ -0,0 +1,11 @@
+# %cd /content/drive/MyDrive/005_BOKTIAR_AHMED_BAPPY/YouTube/GANs-Practical
+
+# Commented out IPython magic to ensure Python compatibility.
+# %pwd
+
+!git clone https://github.com/xinntao/ESRGAN.git
+
+# Commented out IPython magic to ensure Python compatibility.
+# %cd ESRGAN
+
+!python test.py
